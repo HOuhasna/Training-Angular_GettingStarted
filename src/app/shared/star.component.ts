26a956c99684ed2dee71552773549555f3a54b47
houhasna <chemmakh@gmail.com>
2019-01-08 17:18:49 +0100

    End of chapter 08 - Add Nested component : star rating.

---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnChanges, Input, Output, EventEmitter } from "@angular/core";
+
+
+@Component({
+    selector : "pm-star"
+    ,templateUrl : "./star.component.html"
+    ,styleUrls : ["./star.component.css"]
+})
+export class StarComponent implements OnChanges{
+    
+    @Input()
+    rating : number = 4;
+    starWidth : number = 4 * 75 / 5;
+
+    ngOnChanges() : void{
+        this.starWidth = this.rating * 75 / 5;
+    }
+
+    @Output()
+    ratingClicked : EventEmitter<string> = new EventEmitter<string>();
+
+    onClick():void {
+        
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+    }
+}
